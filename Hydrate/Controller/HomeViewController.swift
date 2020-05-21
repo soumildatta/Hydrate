f139cfd3b8d08b5e15373b9cad8482ab3aa7de81
Soumil Datta <soumildatta@gmail.com>
2020-05-21 14:25:27 -0500

    Implement basic functionality using a stepper

--- Hydrate/Controller/HomeViewController.swift
+++ Hydrate/Controller/HomeViewController.swift
@@ -10,25 +10,49 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
-    @IBOutlet weak var currentGlasses: UILabel!
+    @IBOutlet weak var currentLabel: UILabel!
+    @IBOutlet weak var goalLabel: UILabel!
+    @IBOutlet weak var percentLabel: UILabel!
+    @IBOutlet weak var waterBar: UIProgressView!
     
-    var current = 0
-    var goal = 8
+    var current: Float = 0.0
+    var goal: Float = 8.0
+    var percent: Float = 0.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // set number of glasses consumed today here
-        currentGlasses.text = "\(current)/\(goal)"
+        // making progress bar vertical and increasing width
+        waterBar.transform = waterBar.transform.rotated(by: 270 * .pi / 180)
+        waterBar.transform = waterBar.transform.scaledBy(x: 1.8, y: 100)
+
+        // iniatialize progressbar
+        percent = current / goal
+        waterBar.progress = percent
+        percentLabel.text = String(format: "%.0f", percent * 100) + "%"
+
+        
+        // set goal and number of glasses consumed today here
+        goalLabel.text = String(format: "Current Daily Goal: %.0f glasses", goal)
+        currentLabel.text = String(format: "%.0f/%.0f", current, goal)
     }
     
     @IBAction func glassesStepper(_ sender: UIStepper) {
         // testing stepper
         
         // for now, do not let user drink more than goal amount (percentage purposes)
+        
         if current < goal {
-            currentGlasses.text = String(format: "%.0f/\(goal)", sender.value)
-            current += 1
+            // change current value based on stepper
+            current = Float(sender.value)
+            
+            currentLabel.text = String(format: "%.0f/%.0f", current, goal)
+            
+            // progress bar and percent progression
+            percent = current / goal
+            waterBar.progress = percent
+            percentLabel.text = String(format: "%.0f", percent * 100) + "%"
+            
         }
         
     }
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Hydrate App (iOS)
+
+Hydrate is a simple app that helps you track your water consumption per day. The water consumption is measured by cups, and the consumption goal everyday is customizable.
