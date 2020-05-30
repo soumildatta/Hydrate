ea0f8837a75f34d0b3a72f91b307e1e99ca4eb29
Soumil Datta <soumildatta@gmail.com>
2020-05-29 21:55:43 -0500

    Show error if email field empty

--- Hydrate/Controller/ForgotViewController.swift
+++ Hydrate/Controller/ForgotViewController.swift
@@ -25,14 +25,18 @@ class ForgotViewController: UIViewController {
     
     @IBAction func submitPressed(_ sender: UIButton) {
         
-        if let email = emailTextField.text {
-            Auth.auth().sendPasswordReset(withEmail: email, completion: nil)
+        if emailTextField.text != "" {
+            if let email = emailTextField.text {
+                Auth.auth().sendPasswordReset(withEmail: email, completion: nil)
+            }
+            
+            let alertController = UIAlertController(title: "Password reset email sent", message: "If your email matches one in our database, we will send you the link to reset your password", preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "Dismiss", style: .destructive))
+            
+            self.present(alertController, animated: true, completion: nil)
+        } else{
+            warningLabel.text = "Enter an email!"
         }
-        
-        let alertController = UIAlertController(title: "Password reset email sent", message: "If your email matches one in our database, we will send you the link to reset your password", preferredStyle: .alert)
-        alertController.addAction(UIAlertAction(title: "Dismiss", style: .destructive))
-        
-        self.present(alertController, animated: true, completion: nil)
         
     }
 }
